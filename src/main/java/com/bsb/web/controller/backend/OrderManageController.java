514a33a9f492061a110451961148846ab66d7ac7
challengerzsz <challengerzsz@126.com>
2018-10-13 21:03:11 +0800

    配置身份权限验证拦截器 重构管理员逻辑代码

--- src/main/java/com/bsb/web/controller/backend/OrderManageController.java
+++ src/main/java/com/bsb/web/controller/backend/OrderManageController.java
@@ -1,22 +1,13 @@
 package com.bsb.web.controller.backend;
 
-import com.bsb.common.ResponseCode;
 import com.bsb.common.ServerResponse;
-import com.bsb.util.CookieUtil;
-import com.bsb.util.JsonUtil;
-import com.bsb.util.RedisUtil;
-import com.bsb.web.pojo.User;
 import com.bsb.web.service.IOrderService;
-import com.bsb.web.service.IUserService;
 import com.bsb.web.vo.OrderVo;
 import com.github.pagehelper.PageInfo;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author zeng
@@ -28,98 +19,33 @@ public class OrderManageController {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    private IUserService userService;
-    @Autowired
     private IOrderService orderService;
-    @Autowired
-    private RedisUtil redisUtil;
+
 
     @GetMapping("/list")
-    public ServerResponse<PageInfo> orderList(HttpServletRequest request,
-                                              @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+    public ServerResponse<PageInfo> orderList(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
                                               @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
-        String loginToken = CookieUtil.readLoginToken(request);
-        logger.error("error {}", loginToken);
-        if (StringUtils.isEmpty(loginToken)) {
-            return ServerResponse.createByErrorMsg("用户未登录");
-        }
-        String userJson = redisUtil.getRedisValue(loginToken);
-        User user = JsonUtil.stringToObj(userJson, User.class);
-        if (user == null) {
-            return ServerResponse.createByErrorCodeMsg(ResponseCode.NEED_LOGIN.getCode(), "用户未登录");
-        }
 
-        if (userService.checkAdminRole(user).isSuccess()) {
-            //管理员权限
-            return orderService.manageList(pageNum, pageSize);
-        } else {
-            return ServerResponse.createByErrorMsg("无权限操作，需要管理员权限");
-        }
+        return orderService.manageList(pageNum, pageSize);
     }
 
     @GetMapping("/getDetail")
-    public ServerResponse<OrderVo> getDetail(HttpServletRequest request, Long orderNo) {
+    public ServerResponse<OrderVo> getDetail(Long orderNo) {
 
-        String loginToken = CookieUtil.readLoginToken(request);
-        logger.error("error {}", loginToken);
-        if (StringUtils.isEmpty(loginToken)) {
-            return ServerResponse.createByErrorMsg("用户未登录");
-        }
-        String userJson = redisUtil.getRedisValue(loginToken);
-        User user = JsonUtil.stringToObj(userJson, User.class);
-        if (user == null) {
-            return ServerResponse.createByErrorCodeMsg(ResponseCode.NEED_LOGIN.getCode(), "用户未登录");
-        }
-
-        if (userService.checkAdminRole(user).isSuccess()) {
-            //管理员权限
-            return orderService.manageDetail(orderNo);
-        } else {
-            return ServerResponse.createByErrorMsg("无权限操作，需要管理员权限");
-        }
+        return orderService.manageDetail(orderNo);
     }
 
     @PostMapping("/search")
-    public ServerResponse<PageInfo> search(HttpServletRequest request, Long orderNo,
-                                          @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
-                                          @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
-        String loginToken = CookieUtil.readLoginToken(request);
-        logger.error("error {}", loginToken);
-        if (StringUtils.isEmpty(loginToken)) {
-            return ServerResponse.createByErrorMsg("用户未登录");
-        }
-        String userJson = redisUtil.getRedisValue(loginToken);
-        User user = JsonUtil.stringToObj(userJson, User.class);
-        if (user == null) {
-            return ServerResponse.createByErrorCodeMsg(ResponseCode.NEED_LOGIN.getCode(), "用户未登录");
-        }
+    public ServerResponse<PageInfo> search(Long orderNo, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                           @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
 
-        if (userService.checkAdminRole(user).isSuccess()) {
-            //管理员权限
-            return orderService.manageSearch(orderNo, pageNum, pageSize);
-        } else {
-            return ServerResponse.createByErrorMsg("无权限操作，需要管理员权限");
-        }
+        return orderService.manageSearch(orderNo, pageNum, pageSize);
+
     }
 
     @GetMapping("/sendGoods")
-    public ServerResponse<String> sendGoods(HttpServletRequest request, Long orderNo) {
-        String loginToken = CookieUtil.readLoginToken(request);
-        logger.error("error {}", loginToken);
-        if (StringUtils.isEmpty(loginToken)) {
-            return ServerResponse.createByErrorMsg("用户未登录");
-        }
-        String userJson = redisUtil.getRedisValue(loginToken);
-        User user = JsonUtil.stringToObj(userJson, User.class);
-        if (user == null) {
-            return ServerResponse.createByErrorCodeMsg(ResponseCode.NEED_LOGIN.getCode(), "用户未登录");
-        }
+    public ServerResponse<String> sendGoods(Long orderNo) {
 
-        if (userService.checkAdminRole(user).isSuccess()) {
-            //管理员权限
-            return orderService.manageSendGoods(orderNo);
-        } else {
-            return ServerResponse.createByErrorMsg("无权限操作，需要管理员权限");
-        }
+        return orderService.manageSendGoods(orderNo);
     }
 }
